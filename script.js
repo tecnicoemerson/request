23402a27f8b98e118287d9fa191c07d227077741
Emerson Rodrigues <99212639+tecnicoemerson@users.noreply.github.com>
2023-01-17 22:30:44 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 function listaUsuarios(){
     const url = 'http://jsonplaceholder.typicode.com/users';
 
-    fetch(url).then((Response) => {
-        return Response.json();
+    fetch(url).then((response) => {
+        return response.json();
     }).then((data) => {
-        console.log(data)
+        let lista = document.getElementById('lista')
+        data.map((item) => {
+            lista += `<li> ${item.name}</li>`
+        })
     }).catch((error) => error)
 }
 
